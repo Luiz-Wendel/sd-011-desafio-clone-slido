50f47378fd032e1dbf4ea4d9c80ef34284766bc8
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-06-10 18:10:55 -0300

    add question component

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import style from './style.module.css';
 
 export default class Question extends React.Component {
   render() {
+    const { question } = this.props;
+
     return (
-      <section>
-        <h1>New Question</h1>
+      <section className={ style.question }>
+        <p>{ question.username }</p>
+        <p>{ question.questionText }</p>
       </section>
     );
   }
 }
+
+Question.propTypes = {
+  question: PropTypes.shape({
+    questionText: PropTypes.string,
+    username: PropTypes.string,
+  }).isRequired,
+};
